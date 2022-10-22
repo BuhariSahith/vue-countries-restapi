8c13d914f73113c514212e3afa7b5d4bd3587018
buharisahith <buharisahith@gmail.com>
2022-10-22 19:46:57 +0530

    worked on header component and dark-theme functionality

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <HeaderComponent />
+</template>
+
+<script>
+import HeaderComponent from "./components/Header.vue";
+export default {
+  components: {
+    HeaderComponent,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100;400&display=swap");
+
+#app {
+  font-family: "IBM Plex Sans", sans-serif;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: var(--background-color-primary);
+}
+
+/* For light color */
+:root {
+  --background-color-primary: rgba(255, 255, 255, 0.87);
+  --text-primary-color: hsl(200, 15%, 8%);
+}
+
+/* For Dark Color */
+:root.dark-theme {
+  --background-color-primary: #333333;
+  --text-primary-color: #ffffff;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,80 @@
+<template>
+  <header class="shadow-xl h-24 fixed w-full">
+    <div class="flex justify-between p-8">
+      <h1 class="font-bold text-2xl">Countries</h1>
+      <div class="flex cursor-pointer" @click="toggleTheme">
+        <span
+          title="enable light mode"
+          class="dark-mode cursor-pointer"
+          v-show="isSunIconHidden"
+        >
+          <fa icon="fa-solid fa-sun" class="fa-2x" />
+        </span>
+        <span
+          title="enable dark mode"
+          class="dark-mode cursor-pointer"
+          v-show="isMoonIconHidden"
+        >
+          <fa icon="fa-solid fa-moon" class="fa-2x" />
+        </span>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isSunIconHidden: false,
+      isMoonIconHidden: true,
+      userTheme: "light-theme",
+    };
+  },
+
+  mounted() {
+    const initUserTheme = this.getTheme() || this.getMediaPrefernce();
+    this.setTheme(initUserTheme);
+  },
+
+  methods: {
+    toggleTheme() {
+      console.log("clicked");
+      const activateTheme = localStorage.getItem("user-Theme");
+      if (activateTheme === "light-theme") {
+        this.isMoonIconHidden = false;
+        this.isSunIconHidden = true;
+        this.setTheme("dark-theme");
+      } else {
+        this.isMoonIconHidden = true;
+        this.isSunIconHidden = false;
+        this.setTheme("light-theme");
+      }
+    },
+
+    setTheme(theme) {
+      localStorage.setItem("user-Theme", theme);
+      this.userTheme = theme;
+      document.documentElement.className = theme;
+    },
+
+    getTheme() {
+      return localStorage.getItem("user-Theme");
+    },
+
+    getMediaPrefernce() {
+      const hasMediaPrefrences = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      return hasMediaPrefrences ? "dark-theme" : "light-theme";
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1,
+.dark-mode {
+  color: var(--text-primary-color);
+}
+</style>
