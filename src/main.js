d4128ba80d7808d5238cf431790828396073ffb1
buharisahith <buharisahith@gmail.com>
2022-10-22 19:08:14 +0530

    added vue-fontawesome icons as fa-components

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
+/* eslint-disable vue/multi-word-component-names */
 import { createApp } from "vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import App from "./App.vue";
 import router from "./router";
-
-import "./assets/main.css";
+import "./styles/app.css";
 
 const app = createApp(App);
-
+library.add(fas);
+app.component("fa", FontAwesomeIcon);
 app.use(router);
-
 app.mount("#app");
