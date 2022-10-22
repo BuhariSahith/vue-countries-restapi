143396c62cd54a2664e586621cf51ff0d2b30a6c
buharisahith <buharisahith@gmail.com>
2022-10-22 16:54:34 +0530

    re-configured tailwind css and added prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-export const content = ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
+/* eslint-disable no-undef */
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
