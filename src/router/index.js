07c6f1d13152db25b3e50ed7b11626f8bfc25616
buharisahith <buharisahith@gmail.com>
2022-10-24 11:07:20 +0530

    added rout for single page view4

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../pages/HomePage.vue";
+import SingleCountryDetail from "../pages/SingleCountryDetail.vue"
 
 const routes = [
   {
     path: "/",
     name: "HomePageView",
     component: HomePage,
+  },
+  {
+    path: "/country/:name",
+    name: "SingleCountryDetail",
+    component: SingleCountryDetail,
+    props: true, // If we are passing params have to do this true
   },
 
   // route level code-splitting
